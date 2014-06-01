4807de9ea225bf7b715fa88beca7e9b184f8d4ad
hannwoei <hannwoei_ho@hotmail.com>
2014-06-02 00:30:04 +0200

    change image capture settings to v4l2

--- modules/OpenCV/opencv_module.c
+++ modules/OpenCV/opencv_module.c
@@ -47,6 +47,24 @@ void opencv_module_run(void) {
   }
 }
 
+/////////////////////////////////////////////////////////////////////////
+// COMPUTER VISION THREAD
+
+// Video
+#include "v4l/video.h"
+#include "resize.h"
+
+// Payload Code
+#include "opencv_code.h"
+
+//#define DOWNLINK_VIDEO 1
+
+#ifdef DOWNLINK_VIDEO
+#include "encoding/jpeg.h"
+#include "encoding/rtp.h"
+#endif
+
+#include <stdio.h>
 
 pthread_t computervision_thread;
 volatile uint8_t computervision_thread_status = 0;
@@ -54,23 +72,74 @@ volatile uint8_t computer_vision_thread_command = 0;
 void *computervision_thread_main(void* data);
 void *computervision_thread_main(void* data)
 {
-  //init
-  IplImage* img = 0;
-  IplImage* gray_img = 0;
-  img = cvLoadImage("/data/video/example.jpg",CV_LOAD_IMAGE_COLOR);
-  gray_img = cvCreateImage( cvGetSize(img), IPL_DEPTH_8U, 1);
-  cvCvtColor( img, gray_img, CV_RGB2GRAY);
-  cvSaveImage("/data/video/gray_example.jpg",gray_img, 0);
+	//start timer using volatile variable (IRQ)
+  // Video Input
+  struct vid_struct vid;
+  vid.device = (char*)"/dev/video2"; // video1 = front camera; video2 = bottom camera
+  vid.w=320; // front camera = 1280; bottom camera = 320
+  vid.h=240;  // front camera = 720; bottom camera = 240
+  vid.n_buffers = 4;
+  if (video_init(&vid)<0) {
+    printf("Error initialising video\n");
+    computervision_thread_status = -1;
+    return 0;
+  }
+
+  // Video Grabbing
+  struct img_struct* img_new = video_create_image(&vid);
+
+  // Video Resizing
+  #define DOWNSIZE_FACTOR   1
+  struct img_struct small;
+  small.w = vid.w / DOWNSIZE_FACTOR;
+  small.h = vid.h / DOWNSIZE_FACTOR;
+  small.buf = (uint8_t*)malloc(small.w*small.h*2);
+
+#ifdef DOWNLINK_VIDEO
+
+  // Video Compression
+  uint8_t* jpegbuf = (uint8_t*)malloc(vid.h*vid.w*2);
+
+  // Network Transmit
+  struct UdpSocket* vsock;
+  //#define FMS_UNICAST 0
+  //#define FMS_BROADCAST 1
+  vsock = udp_socket("192.168.1.255", 5000, 5001, FMS_BROADCAST);
+
+#endif
+
+  // First Apply Settings before init
+  imgWidth = small.w;
+  imgHeight = small.h;
+  verbose = 2;
+  my_plugin_init();
 
   while (computer_vision_thread_command > 0)
   {
+    video_grab_image(&vid, img_new);
+
+    // Resize: device by 4
+    resize_uyuv(img_new, &small, DOWNSIZE_FACTOR);
 
     // Process
+    my_plugin_run(small.buf);
 
+#ifdef DOWNLINK_VIDEO
+    // JPEG encode the image:
+    uint32_t quality_factor = 60; // quality factor from 1 (high quality) to 8 (low quality)
+    uint8_t dri_header = 0;
+    uint32_t image_format = FOUR_TWO_TWO;  // format (in jpeg.h)
+    uint8_t* end = encode_image (small.buf, jpegbuf, quality_factor, image_format, small.w, small.h, dri_header);
+    uint32_t size = end-(jpegbuf);
+
+    printf("Sending an image ...%u\n",size);
+    uint32_t delta_t_per_frame = 0; // 0 = use drone clock
+    send_rtp_frame(vsock, jpegbuf,size, small.w, small.h,0, quality_factor, dri_header, delta_t_per_frame);
+#endif
     computervision_thread_has_results++;
   }
   printf("Thread Closed\n");
-  
+  video_close(&vid);
   computervision_thread_status = -100;
   return 0;
 }
